c6bf68720983f8ebc280a1376cb1accd31911e74
Johan Wirén <johan.wiren.se@gmail.com>
2015-11-23 20:10:16 +0100

    Refactor filtering

--- lambda/rssreader.py
+++ lambda/rssreader.py
@@ -17,7 +17,14 @@ TABLE = boto.dynamodb2.table.Table('rssfeedkindle-dynamoDbTable-IWHWI0I2NY0K',
             aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'],
             security_token=os.environ.get('AWS_SESSION_TOKEN',None)))
 
-def filter_item(feed_item):
+
+def entry_is_new(entry):
+    if TABLE.has_item(itemId=entry.link):
+        return False
+    return True
+
+
+def entry_is_wanted(entry):
     NOT_WANTED = [
             'Hobbies and Interests',
             'Makeup',
@@ -25,37 +32,33 @@ def filter_item(feed_item):
             'Food and Drink',
             'Recipes',
             ]
-    for tag in feed_item['tags']:
-        if tag in NOT_WANTED:
+    for tag in entry.tags:
+        if tag.term in NOT_WANTED:
             return False
     return True
 
 
-def get_feed_links(url):
-    feed = feedparser.parse(url)
-    return [x.link for x in feed.entries if filter_item(x)]
-
-
-def get_new_links(links):
-    return [new_link for new_link in [
-        link for link in links if not TABLE.has_item(itemId=link)]]
-
 def handle_feed(url):
+    feed = feedparser.parse(url)
     ses = boto.ses.connect_to_region(
             'eu-west-1',
             aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
             aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'],
             security_token=os.environ.get('AWS_SESSION_TOKEN',None))
-    links = get_feed_links(url)
-    new_links = get_new_links(links)
-    result = []
-    for link in new_links:
-        ses.send_email(SENDER_EMAIL,
-                'new item for my kindle',
-                link,
-                [RECIPIENT_EMAIL])
-        TABLE.put_item(data={'itemId': link})
-        result.append(link)
+    result = dict(sent=[], skipped=[], old=[])
+    for entry in feed.entries:
+        if entry_is_wanted(entry):
+            if entry_is_new(entry):
+                ses.send_email(SENDER_EMAIL,
+                        'new item for my kindle',
+                        entry.link,
+                        [RECIPIENT_EMAIL])
+                TABLE.put_item(data={'itemId': entry.link})
+                result['sent'].append(entry.title)
+            else:
+                result['old'].append(entry.title)
+        else:
+            result['skipped'].append({'title': entry.title, 'tags': entry.tags})
     return result
 
 
